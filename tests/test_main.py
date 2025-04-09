c680b3f9056af780785bad6858ae8c4a8ac50975
gcgbarbosa <gcgbarbosa@gmail.com>
2025-04-09 14:53:59 +0200

    build: add code coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 from template.app import main
+import runpy
+import sys
 
 
 def test_main_output_and_return(capsys):
@@ -8,3 +10,12 @@ def test_main_output_and_return(capsys):
     assert "Hello from template!" in captured.out
 
     assert result == 2
+
+
+def test_main_block():
+    # Remove the module from sys.modules if it exists.
+    module_name = "template.app"  # adjust the module path as needed
+    if module_name in sys.modules:
+        del sys.modules[module_name]
+
+    runpy.run_module(module_name, run_name="__main__")
