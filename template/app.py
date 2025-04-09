3bcfe17fb4747f2606e2f5d510083636955e1a4b
gcgbarbosa <gcgbarbosa@gmail.com>
2025-04-09 15:21:21 +0200

    build: interrogate options

--- template/app.py
+++ template/app.py
@@ -1,4 +1,16 @@
+"""Entrypoint"""
+
+
 def main():
+    """
+    Main function that prints a greeting message and returns the sum of 1 and 1.
+
+    This function outputs a simple greeting to the console and computes a basic arithmetic
+    operation.
+
+    Returns:
+        int: The result of the arithmetic operation (1 + 1), which is 2.
+    """
     print("Hello from template!")
     return 1 + 1
 
