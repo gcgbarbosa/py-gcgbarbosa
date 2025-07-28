554b3d3e81ae5edab3f3e6a8f6e33ab33f626502
gcgbarbosa <gcgbarbosa@gmail.com>
2025-07-28 15:00:31 +0200

    fix(tests): update import paths from template to src
    Updated the import paths in test_main.py to reflect the new module structure, changing imports from `template.app` to `src.app`.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from template.app import main
+from src.app import main
 import runpy
 import sys
 
@@ -14,7 +14,7 @@ def test_main_output_and_return(capsys):
 
 def test_main_block():
     # Remove the module from sys.modules if it exists.
-    module_name = "template.app"  # adjust the module path as needed
+    module_name = "src.app"  # adjust the module path as needed
     if module_name in sys.modules:
         del sys.modules[module_name]
 
